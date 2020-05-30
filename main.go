24e4303e6780de48f462bf623094d1ca3d565aa9
allentom <qweasdzxczse2@gmail.com>
2020-05-30 16:01:22 +0900

    change port

--- main.go
+++ main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"os"
 )
 
 func main() {
 	r := gin.Default()
 	r.Static("/static", "./static")
-	r.Run(":8885") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	port := os.Getenv("PORT")
+	r.Run(":" + port) // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
